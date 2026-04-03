74141578383d586b5e83f8492ece043f3ca4d68c
oviya <oviyarajagopal21@gmail.com>
2026-04-03 19:28:10 +0530

    Initial commit

---
+++ app/utils/security.py
@@ -0,0 +1,35 @@
+from passlib.context import CryptContext
+from jose import jwt
+from jose import JWTError
+from datetime import datetime, timedelta
+
+SECRET_KEY = "mysecretkey"   # later we secure this
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+ #🔑 Create JWT token
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+
+        if email is None:
+            return None
+
+        return email
+    except JWTError:
+        return None
